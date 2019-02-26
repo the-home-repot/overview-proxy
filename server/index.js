b20fc606fa40c08245115d9e79417dd1f7610b86
Remington Davis <remy@Remingtons-MacBook-Pro.local>
2019-02-26 14:31:25 -0600

    Functional Local Proxy

--- server/index.js
+++ server/index.js
@@ -12,15 +12,19 @@ app.use( bodyParser.urlencoded({ extended: true }) );
 app.get('/products/:ID', (req, res) => {
   let id = req.params.ID;
   axios.get(`http://ec2-3-93-54-61.compute-1.amazonaws.com/products/${id}`)
+  axios.get(`http://ec2-3-93-54-61.compute-1.amazonaws.com/products/${id}`)
     .then(({data}) => {
       res.send(JSON.stringify(data));
     })
 })
 app.get('/productinfo/:ID', (req, res) => {
+  console.log('new hit')
   let id = req.params.ID;
+  console.log(id);
   axios.get(`http://ec2-13-59-174-32.us-east-2.compute.amazonaws.com:3030/productinfo/${id}`)
+  // axios.get(`http://localhost:3030/productinfo/${id}`)
     .then(({data}) => {
-      res.send(JSON.stringify(data));
+      res.send(data);
     })
 })
 app.get('/getFiveRandom', (req, res) => {
